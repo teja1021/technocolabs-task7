c3f7cbe5644e0a1cdfed4fb138e0ab175cd0736b
teja varma alluri <153795647+teja1021@users.noreply.github.com>
2025-05-05 18:27:55 +0530

    Update task7.js

---
+++ task7.js
@@ -0,0 +1,8 @@
+
+const myAge = 21; // Human age
+let earlyYears = 2 * 10.5; // First 2 years in dog years
+let laterYears = (myAge - 2) * 4; // Remaining years in dog years
+let myAgeInDogYears = earlyYears + laterYears; // Total dog years
+let myName = "Teja Varma".toLowerCase(); // Name in lowercase
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
